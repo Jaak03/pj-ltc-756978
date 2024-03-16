720d30467058b9eef5436cdf8150e7a3e01a1849
🌻 jaak 🌻 <jacques@mohara.co>
2024-03-16 06:12:59 +0000

    feat: got the first test to pass

---
+++ challenge/index.test.ts
@@ -0,0 +1,11 @@
+import { test } from './index';
+describe('index', () => {
+  it('should call the function', () => {
+    try {
+      test();
+      expect(true).toBe(true)
+    } catch (error) {
+      console.log('error', error);
+    }
+  })
+})
